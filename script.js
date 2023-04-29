08bfadc9db0572c662dc0bed4728cea549f1afee
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-04-29 11:30:29 +0100

    create a div and append children to it in js

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ header3.textContent = "I'm a blue h3";
 header3.style.color = "blue";
 
 element.appendChild(header3);
+
+const div = document.createElement("div");
+
+const header1 = document.createElement("h1");
+header1.textContent = "I'm in a div";
+div.appendChild(header1);
+
+const para2 = document.createElement("p");
+para2.textContent = "ME TOO!";
+div.appendChild(para2);
+
+element.appendChild(div);
